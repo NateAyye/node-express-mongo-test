b45b8b17a10d74e16ddc701203f441a771794513
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-03-05 21:02:10 -0800

    added the mongoose strictQuery feild for future compatability

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    await mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.DATABASE_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
